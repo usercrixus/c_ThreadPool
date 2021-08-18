76f98c85f34fd56fee863341bf4848ec382bec9c
Supra <70440132+UserSupra@users.noreply.github.com>
2021-08-18 21:49:50 +0200

    Update threadpool.c

--- ProgrammingProject-7-1/threadpool.c
+++ ProgrammingProject-7-1/threadpool.c
@@ -8,7 +8,7 @@
 int enqueue(Task* t, ThreadPool* threadPool)
 {
     pthread_mutex_lock(&threadPool->dequeueMutex); // protected area
-
+    
     if(threadPool->head == NULL){
         threadPool->head = t; // assign task to the pool
     }else{
@@ -19,14 +19,14 @@ int enqueue(Task* t, ThreadPool* threadPool)
         buff->next = t; // put task at the end of the pool
     }
     pthread_cond_signal(&threadPool->cond); // try to wake up a thread for the new task
-
+    
     pthread_mutex_unlock(&threadPool->dequeueMutex); // end of protected area
     return 0;
 }
 
 Task* dequeue(ThreadPool* threadPool){
     pthread_mutex_lock(&threadPool->dequeueMutex); // protected area
-
+    
     while(threadPool->head == NULL && threadPool->active == true){
         pthread_cond_wait(&threadPool->cond, &threadPool->dequeueMutex); // if pool is null we cant dequeue so we wait
     }
@@ -34,28 +34,28 @@ Task* dequeue(ThreadPool* threadPool){
         pthread_mutex_unlock(&threadPool->dequeueMutex); // if pool is close and null we return null as we broke the thread while loop condition
         return NULL;
     }
-
+    
     Task* worktodo = threadPool->head;
     if(threadPool->head != NULL){
         threadPool->head = threadPool->head->next; // if all is right we dequeue
     }
-
+    
     pthread_mutex_unlock(&threadPool->dequeueMutex); // end of protected area
-
+    
     return worktodo; // return the head of queue.
 }
 
 
 void *worker(void *param){
-
+    
     ThreadPool* threadPool = (ThreadPool*)param; // convert param to ThreadPool
     Task* worktodo; // this is the object who will be dequeue
     while((worktodo = dequeue(threadPool)) != NULL){ // if dequeue return null, pool_shutdown have been call and the pool is empty so, we leave the thread
-    printf("thread id : %lu | ", pthread_self());
+        printf("thread id : %lu | ", pthread_self());
         execute(worktodo->function, worktodo->data); // execute function with params
         free(worktodo); // avoid memory leaks
     }
-
+    
     pthread_exit(0); // close thread
 }
 void execute(void (*somefunction)(void *p), void *p){
@@ -70,7 +70,7 @@ int pool_submit(void (*somefunction)(void *p), void *p, ThreadPool* threadPool){
         t->next = NULL; // it will be push at the end of queue
         enqueue(t, threadPool); // push to the queue
     }
-
+    
     return 0;
 }
 
@@ -81,18 +81,18 @@ ThreadPool* pool_init(int size)
     threadPool->active = true; // make it active
     pthread_mutex_init(&threadPool->dequeueMutex, NULL); // init the queue mutex
     pthread_cond_init(&threadPool->cond, NULL); // init the stop and go condition
-
+    
     pthread_attr_t attr; // attr to the threads
     pthread_attr_init(&attr);
-
+    
     pthread_t* pool = malloc(sizeof(pthread_t)*size); // create pool of thread of desired size
     threadPool->pool = pool; // save the pool
     threadPool->size = size; // save the size
-
+    
     for(int i = 0 ; i < size ; i++){
         pthread_create(&pool[i], &attr, worker, threadPool); // launch all thread
     }
-
+    
     return threadPool; // return the pool
 }
 
@@ -105,5 +105,5 @@ void pool_shutdown(ThreadPool* threadPool)
     }
     free(threadPool->pool); // prevent memory leaks
     free(threadPool); // prevent memory leaks
-
+    
 }
